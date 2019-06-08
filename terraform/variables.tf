729086506032b59c220ab081dafaf3dcfacf51d5
Soul_in_Gun <none>
2019-06-08 16:13:03 +0300

    On branch terraform-2 - made homework with no additional tasks

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,26 +4,10 @@ variable project {
 
 variable region {
   description = "Region"
-  default     = "us-west1"
+  default     = "europe-west1"
 }
 
 variable zone {
   description = "Zone"
-  default     = "b"
-}
-
-variable public_key_path {
-  description = "Path to the public key used for ssh access"
-}
-
-variable private_key_path {
-  description = "Path to the private key used for provisioner ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
-}
-
-variable "count" {
-  default = "1"
+  default     = "europe-west1-b"
 }
